398cc32398cc6072af225db43a3d4bfcb3e1c18d
indrajithathukorala <nalinathukoralanalin@gmail.com>
2022-10-31 23:40:39 +0530

    My First Commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+import 'package:iq_quiz/screens/my_screen.dart';
+
+Future<void> backgroundHabdler(RemoteMessage message) async {
+  print(message.data.toString());
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundHabdler);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        backgroundColor: Colors.purpleAccent,
+      ),
+      home: const MyScreeen(),
+    );
+  }
+}
---
+++ lib/screens/my_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyScreeen extends StatelessWidget {
+  const MyScreeen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("dsdsds"),
+      ),
+    );
+  }
+}
